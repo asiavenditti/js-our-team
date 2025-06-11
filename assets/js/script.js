7cd6de84123671ee58ab7218301b800f180ca2ea
Your Name <you@example.com>
2025-06-11 17:25:38 +0200

    add function to render team member cards with template literals

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,36 +3,77 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "./assets/img/male1.png"
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "./assets/img/female1.png"
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "./assets/img/male2.png"
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "./assets/img/female2.png"
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "./assets/img/male3.png"
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
+    img: "./assets/img/female3.png"
   }
 ];
+
+
+
+// funzione per creare una card dinamica
+
+
+let members = ''
+
+const divEl = document.getElementById('team')
+
+const cardMaker = (teamMembers) => {
+
+
+  for (let i = 0; i < teamMembers.length; i++) {
+
+    const member = teamMembers[i]
+
+    const card = `
+    <div class="col-md-4 mb-4">
+    <div class="card" style="max-width: 540px;">
+    <div class="row g-0">
+    <div class="col-md-4">
+            <img src="${member.img}" class="img-fluid object-fit-cover rounded-start" alt="member">
+            </div>
+          <div class="col-md-8">
+            <div class="card-body text-bg-dark">
+              <h5 class="card-title mb-1">${member.name}</h5>
+              <p class="description mb-1">${member.role}</p>
+              <span class="email mb-1">${member.email}</span>
+              </div>
+              </div>
+            </div>
+            </div>
+            </div>`
+    members += card
+  }
+
+  return members
+}
+divEl.innerHTML = cardMaker(teamMembers)
+
